29b66d6a5d1a3f17ff362c94be50778d15cba9e3
RocqJones <jonesmbindyo@gmail.com>
2021-05-13 10:45:15 +0300

    first web app

---
+++ 1WebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace _1WebApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string datetime = DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = datetime;
+        }
+    }
+}
