97c455ecab57d8627bc35eddfbbc93208508d07e
RocqJones <jonesmbindyo@gmail.com>
2021-06-04 11:16:05 +0300

    add model class

--- ExampleWebAPI/Models/TodoItem.cs
+++ ExampleWebAPI/Models/TodoItem.cs
@@ -2,6 +2,7 @@ namespace ExampleWebAPI.Models
 {
     public class TodoItem
     {
+        // The Id property functions as the unique key in a relational database.
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
