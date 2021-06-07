59bb094709ad7c96fcd90355cca729966e96ed94
RocqJones <jonesmbindyo@gmail.com>
2021-06-07 14:55:19 +0300

    country code model

---
+++ CRB/Models/CountryCodeModel.cs
@@ -0,0 +1,9 @@
+namespace CRB.Models
+{
+    public class CountryCodeModel
+    {
+        public int InternalItemCode { get; set; }
+        public string ItemValue { get; set; }
+        public string Description { get; set; }
+    }
+}   
