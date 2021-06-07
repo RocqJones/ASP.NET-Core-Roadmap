2013af278b7833e12dc4bf82fe0109b1a1536313
RocqJones <jonesmbindyo@gmail.com>
2021-06-07 11:22:16 +0300

    loan data model

--- CRB/Models/LoanData.cs
+++ CRB/Models/LoanData.cs
@@ -2,11 +2,24 @@ namespace CRB.Models
 {
     public class LoanData
     {
-        public long Id { get; set; }
-        public string LoanPurpoe { get; set; }
-        public string Dispute { get; set; }
-        public string Dates { get; set; }
-        public string Collaterals { get; set; }
-        public string FeesAndPenalties { get; set; }
+        public long OurBranchID { get; set; }
+        public long AccountID { get; set; }
+        public long LoanSeries { get; set; }
+        public long LoanRefNo { get; set; }
+        public long ApplicationID { get; set; }
+        public long SanctionBy { get; set; }
+        public string SanctionDate { get; set; }
+        public double SanctionAmount { get; set; }
+        public double LoanAmount { get; set; }
+        public double BookedAmount { get; set; }
+        public string ValueDate { get; set; }
+        public int NumberOfDisbursements { get; set; }
+        public string DisbursementModelID { get; set; }
+        public double DisbursementAmount { get; set; }
+        public string DisbursedBy { get; set; }
+        public int Term { get; set; }
+        public string LoanPeriodID { get; set; }
+        public int RepaymentTerm { get; set; }
+        public double InterestRate { get; set; }
     }
 }
