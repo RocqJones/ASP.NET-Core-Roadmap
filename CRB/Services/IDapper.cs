89fa404844d4f836b166c0340bc39307ab26e555
RocqJones <jonesmbindyo@gmail.com>
2021-06-08 08:54:30 +0300

    implemented dapper interface

--- CRB/Services/IDapper.cs
+++ CRB/Services/IDapper.cs
@@ -1,1 +1,19 @@
 // Interface dapper
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+
+namespace Dapper_ORM.Services
+{
+    public interface IDapper : IDisposable
+    {
+        DbConnection GetDbConnection();
+        T Get<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        List<T> GetAll<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        int Execute(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        T Insert<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        T Update<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+    }
+}
