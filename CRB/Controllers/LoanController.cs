3b465ef3054d3daa4e5c8af79bd24c9cf22599a9
RocqJones <jonesmbindyo@gmail.com>
2021-06-09 23:20:21 +0300

    loan controller

--- CRB/Controllers/LoanController.cs
+++ CRB/Controllers/LoanController.cs
@@ -24,16 +24,16 @@ namespace CRB.Controllers
         public async Task<int> Create(LoanDataModel loanData)
         {
             var dbparams = new DynamicParameters();
-            dbparams.Add("AccountID", loanData.AccountID, DbType.Int32);
+            dbparams.Add("AccountID", loanData.AccountID, DbType.Int64);
             var result = await Task.FromResult(_dapper.Insert<int>("r_LoanRepaymentWriter", 
                 dbparams, commandType: CommandType.StoredProcedure));
             return result;
         }
 
-        [HttpGet(nameof(OurBranchID))]
-        public async Task<LoanDataModel> OurBranchID(int OurBranchID)
+        [HttpGet(nameof(GetById))]
+        public async Task<LoanDataModel> GetById(int OurBranchID)
         {
-            var result = await Task.FromResult(_dapper.Get<LoanDataModel>($"SELECT * FROM [t_Loan] WHERE OurBranchID = {OurBranchID}", 
+            var result = await Task.FromResult(_dapper.Get<LoanDataModel>($"SELECT * FROM t_Loan WHERE OurBranchID = {OurBranchID}", 
                 null, commandType: CommandType.StoredProcedure));
             return result;
         }
