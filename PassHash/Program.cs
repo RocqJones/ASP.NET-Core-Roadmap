480ebe96a678715b4ebcd19095e29ead63394524
RocqJones <jonesmbindyo@gmail.com>
2022-01-28 16:21:29 +0300

    hash password to base64

---
+++ PassHash/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PassHash
+{
+    class Program
+    {
+        static public string EncryptText(string strInputText)
+        {
+            byte[] data = Array.ConvertAll<char, byte>(strInputText.ToCharArray(), delegate (char ch) { return (byte)ch; });
+            SHA256 shaM = new SHA256Managed();
+            byte[] result = shaM.ComputeHash(data);
+            return Convert.ToBase64String(result);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EncryptText("admin@domain.compass@102"));
+        }
+    }
+}
